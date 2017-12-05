6f4c86ea35d301f474e31ebd3298bd240af590fc
borja <borja@borja.com>
2017-12-05 19:19:23 +0100

    Subida 6 - El programa no se que hace

--- src/control/Controller.java
+++ src/control/Controller.java
@@ -5,55 +5,37 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.effect.GaussianBlur;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Controller {
-	
-	@FXML
-	private ImageView refugees;
-	
-	
+
 	public void FreeSolo() {
-        try {
-            // Load the fxml file and create a new stage for the popup.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class.getResource("../vista/FreeSolo.fxml"));
-            AnchorPane page = (AnchorPane) loader.load();
-            Stage sendStage = new Stage();
-            sendStage.setTitle("Superheader");
-            
-            //clips
-           
-            refugees = new ImageView();
-//            );
-//    
-//            ColorAdjust colorAdjust = new ColorAdjust();
-//            colorAdjust.setContrast(1);
-//            colorAdjust.setHue(0);
-//            colorAdjust.setBrightness(0);	
-//            colorAdjust.setSaturation(0);
-//            ceiling_image.setEffect(colorAdjust);
-//            ceiling_image.setEffect(new GaussianBlur(5));
-//            refugees.setClip(ceiling);
-            refugees.setEffect(new GaussianBlur(5));
-            
-            Scene scene = new Scene(page);
-             sendStage.setScene(scene);
+		try {
+			// Load the fxml file and create a new stage for the popup.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("../vista/FreeSolo.fxml"));
+			AnchorPane page = (AnchorPane) loader.load();
+			Stage sendStage = new Stage();
+			sendStage.setTitle("FreeSolo");
 
-            
-            sendStage.show();
+			Scene scene = new Scene(page);
+			sendStage.setScene(scene);
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-	
+			sendStage.show();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	@FXML
 	private void handleSend() {
 		FreeSolo();
-		//System.exit(0);
-    }
+	}
+
+	@FXML
+	private void exit() {
+		System.exit(0);
+	}
 }
